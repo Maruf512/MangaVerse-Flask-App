62bafadefb8eabc4baa9bca7760ae7bacc835c40
Maruf512 <marufsarkar512@gmail.com>
2023-04-11 23:17:39 +0600

    home page

--- website/views.py
+++ website/views.py
@@ -5,21 +5,20 @@ views = Blueprint("views", __name__, static_url_path='/static')
 
 @views.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("home.html", Name="The 100 Girlfriends who really really love you", Details="anime details")
 
 
-@views.route('/view', methods=['GET', 'POST'])
+@views.route('/view')
 def view():
     return render_template("view_page.html")
 
 
 @views.route('/next_page')
 def next_page():
-    print("next")
+
     return view()
 
 
 @views.route('/previous_page')
 def previous_page():
-    print("Back")
     return view()
