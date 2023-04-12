dbe6f9056509ad8ac973f0869da6dbd6bdc7f300
Maruf512 <marufsarkar512@gmail.com>
2023-04-12 23:18:17 +0600

    Started working on DB

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
-auth = Blueprint("auth", __name__)
+auth = Blueprint("auth", __name__, static_url_path='/static')
 
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if "@gmail.com" not in email:
+            flash("Invalid Email Adress!!", category="error")
+        elif password1 != password2:
+            flash("First name must be greater then 1 characters.", category="error")
+        elif len(password1) < 7:
+            flash("Password didn't match.", category="error")
+        elif len(firstName) < 2:
+            flash("Password must be at least 7 characters.", category="error")
+        else:
+            # add user to database
+            flash("Account Created!", category="success")
+
     return render_template("sign_up.html")
---
+++ website/models.py
@@ -0,0 +1,24 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Manga_info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    manga_name = db.Column(db.String(1000))
+    manga_type = db.Column(db.String(150))
+    manga_authors = db.Column(db.String(1000))
+    manga_publish_date = db.Column(db.String(150))
+    manga_rating = db.Column(db.String(150))
+    manga_views = db.Column(db.String(150))
+    manga_description = db.Column(db.String(10000))
+    manga_image_id = db.Column(db.String(150))
+
+    upload_date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
