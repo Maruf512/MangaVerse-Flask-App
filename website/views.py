6fab471950ba3b9a154707148471ceb98280843a
Maruf512 <marufsarkar512@gmail.com>
2023-04-13 16:44:25 +0600

    recive files

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-
+import os
+import shutil
 
 views = Blueprint("views", __name__, static_url_path='/static')
+
 
 names = ["Solo Leveling", "Demon Slayer", "Attack on Titan", "Vinland Saga", "Violet Evergarden", "The time i got reincarnated as a slime", "Konosuba",
          "Darling in the Franxx", "Your lie in April", "Your Name", "Hunter x Hunter", "My Hero academia", "One Piece", "The 100 Girlftiends who really really love you"]
@@ -14,21 +16,47 @@ image_link = ["solo_leveling.jpg", "demon_slayer.jpg", "attack_on_titan.jpg", "v
 @views.route('/')
 @login_required
 def home():
-    return render_template("home.html", user=current_user, names_len=len(names), Name=names, images=image_link, Details="anime details")
+    if current_user.email == "marufsarkar512@gmail.com":
+        admin = True
+    else:
+        admin = False
+
+    return render_template("home.html", user=current_user, admin=admin, names_len=len(names), Name=names, images=image_link, Details="anime details")
 
 
 @views.route('/view')
 def view():
-    return render_template("view_page.html", user=current_user)
+    if current_user.email == "marufsarkar512@gmail.com":
+        admin = True
+    else:
+        admin = False
+
+    return render_template("view_page.html", admin=admin, user=current_user)
 
 
 @views.route('manga/<manga_id>')
 def details(manga_id):
-    return render_template("details.html", user=current_user, manga_img=manga_id)
+    if current_user.email == "marufsarkar512@gmail.com":
+        admin = True
+    else:
+        admin = False
+
+    return render_template("details.html", admin=admin, user=current_user, manga_img=manga_id)
 
 
-@views.route('add-manga')
+@views.route('/add-manga', methods=['GET', 'POST'])
 def add_manga():
+
+    if request.method == 'POST':
+        uploaded_file = request.files['cover_img']
+        if uploaded_file.filename != '':
+            img = uploaded_file.filename
+            uploaded_file.save(img)
+            new_path = "D:\Pton\Flask Web App\website\static\\files\Cover_img\\" + img
+            old_path = "D:\Pton\Flask Web App\\" + img
+            shutil.move(old_path, new_path)
+
+        return redirect(url_for('views.add_manga'))
     return render_template("add_manga.html", user=current_user)
 
 
